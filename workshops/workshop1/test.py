3d1ee194c95bd08da68ea69f035d2e4c867ba7a3
effat <effat34@gmail.com>
2025-02-03 12:33:26 -0600

    Add files via upload

--- workshops/workshop1/test.py
+++ workshops/workshop1/test.py
@@ -4,11 +4,14 @@ import source
 
 class TestCalc(unittest.TestCase):
     def testSub1(self):
-        self.assertEqual(1, source.performSub(2, 1), "Bug in implementation. Results should be 1.")    
+        self.assertEqual(1, source.performSub(2, 1), "Bug in implementation. Results should be 1.") 
 
     def testSub2(self):
-        self.assertEqual(10, source.performSub(20, 10), "Bug in implementation. Results should be 1.")    
+        self.assertEqual(1001, source.performSub(2001, 1000), "Bug in implementation. Results should be 1001.") 
+    
+    def testSub3(self):
+        self.assertEqual(-5, source.performSub(2, 7), "Bug in implementation. Results should be -5.") 
 
-
+   
 if __name__ == '__main__': 
-    unittest.main() 
+    unittest.main()

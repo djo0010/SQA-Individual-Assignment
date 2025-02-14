74985be588fe7bded1615553b78c417079fbf0e5
effat <effat34@gmail.com>
2025-02-14 11:25:43 -0600

    Update calc.py

--- workshops/workshop3-taint/calc.py
+++ workshops/workshop3-taint/calc.py
@@ -20,6 +20,6 @@ def simpleCalculator(v1, v2, operation):
 
 
 if __name__=='__main__':
-    val1, val2, op = 1000, 1, '+'
+    val1, val2, op = 1000, 1, '*'
     data = simpleCalculator(val1, val2, op)
     print('Value#1:{} \nValue#2:{} \nOperation:{} \nResult:{}'.format( val1, val2,  op, data  ) )

da51b7b40ecc32304c69e832adc2ee5db6e028d1
effat <effat34@gmail.com>
2025-03-28 10:57:17 -0500

    Add files via upload

--- workshops/workshop 6-forensics/myLogger.py
+++ workshops/workshop 6-forensics/myLogger.py
@@ -3,7 +3,8 @@ import logging
 
 def giveMeLoggingObject():
     format_str = '%(asctime)s:%(message)s'
-    file_name  = 'W10-LOGGER.log'
+    file_name  = 'W6-LOGGER.log'
+    ### creates one Global logger: logging.basicConfig
     logging.basicConfig(format=format_str, filename=file_name, level=logging.INFO)
     loggerObj = logging.getLogger('simple-logger')
     return loggerObj
--- workshops/workshop 6-forensics/simple.py
+++ workshops/workshop 6-forensics/simple.py
@@ -1,15 +1,8 @@
 '''
-Akond Rahman
+
 Simple Python file to demonstrate logging 
 '''
 import numpy as np
-from sklearn.model_selection import KFold
-from sklearn.datasets import load_iris
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import pandas as pd
-from sklearn.metrics import accuracy_score
 import myLogger
 
 
@@ -19,9 +12,8 @@ def simpleDiv(a, b):
 
 if __name__=='__main__': 
    logObj  = myLogger.giveMeLoggingObject()
-   logObj.info("This is for the demonstration of workshop 10.")
+   #logObj.info("This is for the demonstration of workshop 6.")
    print('Welcome to the simple division calculator!')
-   logObj.info("The calculator application has started...")
    a = input('Type in first number: ') 
    b = input('Type in second number: ')
    a , b = int(a), int(b)

909df2a4c14ebd55b1eeeb30f0972db7cc0b7065
Jahidul Arafat <jahidapon@gmail.com>
2025-04-17 12:28:09 -0500

    Update vault4paper.py

--- PROJECT 2025/project/vault4paper.py
+++ PROJECT 2025/project/vault4paper.py
@@ -4,31 +4,33 @@ from venv import create
 import hvac 
 import random 
 
+
+
 '''
+
 1. Install Vault 
 
 - `brew tap hashicorp/tap` 
+
 - `brew install hashicorp/tap/vault` 
 
 2. Verify the HCP Vault installation: 
-
 - `vault` 
 
-3. Start the HCP Vault server. This will help us to programmatically store secrets 
 
+3. Start the HCP Vault server. This will help us to programmatically store secrets 
 - `vault server -dev` 
 
 4. Keep an eye on the output of `vault server -dev` . From the output we will use `address` and `token` 
-
 5. `pip install hvac`
+
 '''
 
 counter_mapper           = {}
-hvac_token               = "REPLACE_THIS_WITH_YOUR_OWN" ## this should come from the output of *vault server -dev* 
-hvac_url                 = "REPLACE_THIS_WITH_YOUR_OWN"        ## this should come from the output of *vault server -dev*
-ansible_secret_retrieval = '"{{ lookup(' + "'hashi_vault', 'secret="  
+hvac_token               = "<paste_your_token_here>" ## this should come from the output of *vault server -dev* 
+hvac_url                 = "http://127.0.0.1:8200"        ## this should come from the output of *vault server -dev*
+ansible_secret_retrieval = '"{{ lookup(' + "'hashi_vault', 'secret=secret/data/"  
 puppet_secret_retrieval  = "Deferred('vault_lookup::lookup', ["  
-
 
 def makeConn():
     hvc_client = hvac.Client(url= hvac_url, token= hvac_token) 
@@ -42,26 +44,27 @@ def storeSecret( client,  secr1 , cnt  ):
 
 def retrieveSecret(client_, cnt_, tech_str): 
     secret_path        = 'SECRET_PATH_' + str( cnt_  )
-    read_response      = client_.secrets.kv.read_secret_version(path=secret_path) 
+    read_response      = client_.secrets.kv.read_secret_version(path=secret_path, raise_on_deleted_version=False) 
     secret_from_vault  = read_response['data']['data']['password']
     # print('The secret we have obtained:')
     print("To retrieve the secret '{}' please plugin the following code snippet in your script:".format( secret_from_vault) )
+
     if tech_str == 'A': 
-        print(ansible_secret_retrieval + secret_path + " token=" + hvac_token + " url=" + hvac_url + ")['data']['data']['password'] }}" + '"')
+        # print(secret_path)
+        # print(ansible_secret_retrieval)
+        print(ansible_secret_retrieval + secret_path + " token=" + hvac_token + " url=" + hvac_url + "')['password'] }}" + '"') # change-01
+
     elif tech_str == 'P':
         # print(puppet_secret_retrieval + '"' + secret_path + '/' + hvac_token  + '", ' + hvac_url + "']),"  )
-        print(puppet_secret_retrieval + '"' + secret_path + '/' + hvac_token  + '", \'' + hvac_url + "']),"  )
-
+        print(puppet_secret_retrieval + '"' + secret_path + '/' + hvac_token  + '", \'' + hvac_url + "']),"  ) # change-02
 
 
 def preprocessTechInput(tech_str):
     str2ret = ''
     tech_str = tech_str.replace('\n', '')
     tech_str = tech_str.replace('\r', '')    
-
     str2ret  = tech_str 
     return str2ret
-
 
 def storeSecrets(lis_secr, tech_str): 
     clientObj    =  makeConn() 
@@ -79,7 +82,6 @@ def retrieveSecrets( tech_str ):
         retrieveSecret( clientObj,  counter, tech_str )
     print('='*50)
 
-
 if __name__ == '__main__': 
     print("Welcome!")
     print("This Python program will ask for inputs from you in order to store secrets and provide code to retrieve secrets.")
@@ -95,12 +97,16 @@ if __name__ == '__main__':
         if secret == 'Q' or secret == 'q': 
             break
         inp_secret_holder.append( secret  )
+
     storeSecrets( inp_secret_holder, technology_string )
     print("Do you want the code snippet to retrieve your secrets? 'Y' for yes and 'N' for no.")
+
     retrieve = input()
     retrieve = preprocessTechInput( retrieve )
     if retrieve == 'Y' or retrieve == 'y': 
         retrieveSecrets( technology_string )
     elif retrieve == 'N' or retrieve == 'n': 
-        print("Thanks for using the program. Goodbye!")
+        print("Thanks for using the program. Goodbye Project!")
+
+
 

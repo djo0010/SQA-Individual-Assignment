20960abf1aa0a8707949b2d7a2bcdae4700665d6
DJ Oakman <djoakman512@gmail.com>
2025-04-23 22:19:17 -0500

    magnum.pp done

--- PROJECT_2025/project/Puppet/magnum.pp
+++ PROJECT_2025/project/Puppet/magnum.pp
@@ -2,7 +2,7 @@
 
   rabbitmq_user { 'magnum':
     admin    => true,
-    password => 'an_even_bigger_secret',
+    password => Deferred('vault_lookup::lookup', ["SECRET_PATH_52287/hvs.OaljJCdZZ56rwmoCOHvWiyPJ", 'http://127.0.0.1:8200']),
     provider => 'rabbitmqctl',
     require  => Class['::rabbitmq'],
   }
@@ -16,7 +16,7 @@
   }
 
   class { '::magnum::db::mysql':
-    password => 'magnum',
+    password => Deferred('vault_lookup::lookup', ["SECRET_PATH_11530/hvs.OaljJCdZZ56rwmoCOHvWiyPJ", 'http://127.0.0.1:8200']),
   }
 
   class { '::magnum::db':
@@ -24,11 +24,11 @@
   }
 
   class { '::magnum::keystone::domain':
-    domain_password => 'oh_my_no_secret',
+    domain_password => Deferred('vault_lookup::lookup', ["SECRET_PATH_46265/hvs.OaljJCdZZ56rwmoCOHvWiyPJ", 'http://127.0.0.1:8200']),
   }
 
   class { '::magnum::keystone::authtoken':
-    password => 'a_big_secret',
+    password => Deferred('vault_lookup::lookup', ["SECRET_PATH_86544/hvs.OaljJCdZZ56rwmoCOHvWiyPJ", 'http://127.0.0.1:8200']),
   }
 
   class { '::magnum::api':
@@ -36,7 +36,7 @@
   }
 
   class { '::magnum::keystone::auth':
-    password     => 'a_big_secret',
+    password     => Deferred('vault_lookup::lookup', ["SECRET_PATH_94249/hvs.OaljJCdZZ56rwmoCOHvWiyPJ", 'http://127.0.0.1:8200']),
     public_url   => 'http://127.0.0.1:9511/v1',
     internal_url => 'http://127.0.0.1:9511/v1',
     admin_url    => 'http://127.0.0.1:9511/v1',
@@ -46,7 +46,7 @@
     rabbit_host         => '127.0.0.1',
     rabbit_port         => '5672',
     rabbit_userid       => 'magnum',
-    rabbit_password     => 'an_even_bigger_secret',
+    rabbit_password     => Deferred('vault_lookup::lookup', ["SECRET_PATH_76569/hvs.OaljJCdZZ56rwmoCOHvWiyPJ", 'http://127.0.0.1:8200']),
     rabbit_use_ssl      =>  false,
     notification_driver => 'messagingv2',
   }
